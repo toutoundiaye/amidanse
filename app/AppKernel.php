20731577bf73ee8c83b962d0d8454f5a8d07baf6
Toutou Baila Ndiaye <bailatoutou.ndiaye@gmail.com>
2017-09-24 19:21:22 +0200

    adding jmslationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
             new AppBundle\AppBundle(),
         ];
 
